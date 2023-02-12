e890994278bbf3dc492b8b075617f25407b1b732
Esteban <estebansalas2502@gmail.com>
2023-02-12 09:53:28 -0600

    Conn

--- Conexiones.vb
+++ Conexiones.vb
@@ -1,3 +1,20 @@
-﻿Module Conexiones
+﻿Module CONEXIONES
+    Public Db As New OleDb.OleDbConnection
+    Public T As New DataSet
+    Public SQL As String
+
+
+    Friend Sub CONECTAR()
+        Dim Ruta As String
+        Ruta = "File Name=" & Application.StartupPath & "\CONEX.udl"
+        Db.ConnectionString = Ruta
+        Db.Open()
+        Exit Sub
+    End Sub
+
+    Friend Sub DESCONECTAR()
+        Db.Close()
+    End Sub
 
 End Module
+
--- Procedimientos.vb
+++ Procedimientos.vb
@@ -1,3 +1,29 @@
-﻿Module Procedimientos
+﻿Module PROCEDIMIENTOS
+    Friend Sub CARGAR_TABLA(ByRef Tabla_Temporal As DataSet, ByVal Sql As String)
+        CONECTAR()
+        Dim Consulta As New OleDb.OleDbDataAdapter(Sql, Db)
+        Consulta.Fill(Tabla_Temporal)
+        DESCONECTAR()
+    End Sub
 
+    Friend Sub EJECUTAR(ByVal Sql As String)
+        CONECTAR()
+        Dim Comando As New OleDb.OleDbCommand(Sql, Db)
+        Comando.ExecuteNonQuery()
+        DESCONECTAR()
+    End Sub
+
+    Function PK(ByVal TABLA As String, ByVal ID As String) As Integer
+        T.Tables.Clear()
+        Sql = "SELECT " & ID & " FROM " & TABLA
+        CARGAR_TABLA(T, Sql)
+        If T.Tables(0).Rows.Count > 0 Then
+            T.Tables.Clear()
+            Sql = "SELECT MAX(ID) FROM " & TABLA
+            CARGAR_TABLA(T, Sql)
+            PK = T.Tables(0).Rows(0).ItemArray(0) + 1
+        Else
+            PK = 1
+        End If
+    End Function
 End Module
