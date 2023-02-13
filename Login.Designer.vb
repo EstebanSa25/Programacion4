beac78af319319e56aa33df74a4c18cdf0b1e127
Esteban <estebansalas2502@gmail.com>
2023-02-12 20:45:41 -0600

    borrado boton

--- Login.Designer.vb
+++ Login.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class PantallaLogin
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class PantallaLogin
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(PantallaLogin))
         Me.Login = New Guna.UI2.WinForms.Guna2HtmlLabel()
@@ -29,7 +29,6 @@ Partial Class PantallaLogin
         Me.IngresarButtom = New Guna.UI2.WinForms.Guna2Button()
         Me.Cerrar = New Guna.UI2.WinForms.Guna2CircleButton()
         Me.RegistroButtom = New Guna.UI2.WinForms.Guna2Button()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Login
@@ -148,21 +147,11 @@ Partial Class PantallaLogin
         Me.RegistroButtom.TabIndex = 8
         Me.RegistroButtom.Text = "Registrarse"
         '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(184, 92)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 9
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
         'PantallaLogin
         '
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(118, Byte), Integer), CType(CType(215, Byte), Integer), CType(CType(196, Byte), Integer))
         Me.ClientSize = New System.Drawing.Size(446, 559)
-        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.RegistroButtom)
         Me.Controls.Add(Me.Cerrar)
         Me.Controls.Add(Me.IngresarButtom)
@@ -184,5 +173,4 @@ Partial Class PantallaLogin
     Friend WithEvents IngresarButtom As Guna.UI2.WinForms.Guna2Button
     Friend WithEvents Cerrar As Guna.UI2.WinForms.Guna2CircleButton
     Friend WithEvents RegistroButtom As Guna.UI2.WinForms.Guna2Button
-    Friend WithEvents Button1 As Button
 End Class
