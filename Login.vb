029fa0121b9cc5b9ccc6f7205e4fda21867918ca
Esteban <estebansalas2502@gmail.com>
2023-02-13 13:32:39 -0600

    validacion_email

--- Login.vb
+++ Login.vb
@@ -6,6 +6,9 @@
             btn_Ingresar.Enabled = False
         End If
     End Sub
+    Function Validar_Correo(Correo As String) As Boolean
+        Return System.Text.RegularExpressions.Regex.IsMatch(Correo, "^(?("")("".+?""@)|(([0-9a-zA-Z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-zA-Z])@))" & "(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,6}))$")
+    End Function
     Private Sub Cerrar_Click(sender As Object, e As EventArgs) Handles Cerrar.Click
         End
     End Sub
@@ -19,16 +22,21 @@
     End Sub
 
     Private Sub IngresarButtom_Click(sender As Object, e As EventArgs) Handles btn_Ingresar.Click
-        VALIDACION_CAMPOS()
-        SQL = "declare @retorno bit;exec @retorno=PR_VALIDAR_LOGIN'" & txt_Correo.Text & "','" & txt_Contraseña.Text & "'select 'LOGIN'=@retorno;"
-        Dim login As Boolean = RETORNAR(SQL)
-        If login.Equals(True) Then
-            MsgBox("Se ingreso al sistema")
+        If (Validar_Correo(txt_Correo.Text).Equals(True)) Then
+            SQL = "declare @retorno bit;exec @retorno=PR_VALIDAR_LOGIN'" & txt_Correo.Text & "','" & txt_Contraseña.Text & "'select 'LOGIN'=@retorno;"
+            Dim login As Boolean = RETORNAR(SQL)
+            If login.Equals(True) Then
+                MsgBox("Se ingreso al sistema")
+            Else
+                MsgBox("Contraseña o correo electronico incorrecto")
+            End If
+
         Else
-            MsgBox("Contraseña o correo electronico incorrecto")
+            MsgBox("Digita un correo electronico valido")
         End If
     End Sub
 
     Private Sub txt_Contraseña_TextChanged(sender As Object, e As EventArgs) Handles txt_Contraseña.TextChanged
+        VALIDACION_CAMPOS()
     End Sub
 End Class
